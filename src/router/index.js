0097b4ea9ddad18bb8e5df66b608b15a808df5d7
_uchanlee <uchanlee.dev@gmail.com>
2020-10-19 02:42:51 +0900

    [UPDATE] <router/index.js> routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { FirstView } from '../components';
+import { FirstView } from '@/components';
 
 Vue.use(Router);
 
@@ -8,7 +8,8 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      name: 'first',
+      path: '/first',
       component: FirstView,
     },
   ],
