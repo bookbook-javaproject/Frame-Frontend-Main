9e0ddc488b5a96425a3ae178ce9ae5b6ca502076
_uchanlee <uchanlee.dev@gmail.com>
2020-10-19 02:29:08 +0900

    [CREATE] <components/FirstView/CommonMain.vue> common component

---
+++ src/components/FirstView/CommonMain.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonMain"
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 0 20.625rem;
+  box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;
+}
+
+@media screen and (max-width: 1540px) {
+  .container {
+    padding: 0 1.875rem;
+  }
+}
+</style>
