b0b5855fff936f998c6413537c23c9c8f238b7d3
Srim <deamoon0801@naver.com>
2020-10-23 02:31:32 +0900

    [UPDATE] <router/index.js> SignUp routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { FirstView } from '@/components';
+import { FirstView, SignUp } from '@/components';
 
 Vue.use(Router);
 
@@ -12,5 +12,10 @@ export default new Router({
       path: '/first',
       component: FirstView,
     },
+    {
+      name : 'signUp',
+      path : '/signUp',
+      component : SignUp
+    }
   ],
 });
