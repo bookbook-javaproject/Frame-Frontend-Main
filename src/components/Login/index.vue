46aca79b37939cc5e25822f64825ed3b892547ee
Srim <deamoon0801@naver.com>
2020-10-23 18:32:22 +0900

    [FIX] add all file merge

---
+++ src/components/Login/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="auth-container">
+        <login />
+        <side-view />
+    </div>
+</template>
+
+<script>
+import { SideView, Login } from './';
+export default {
+    name : "Login",
+    data () {
+        return {
+        }
+    },
+    components : { 'side-view' : SideView, 'login' : Login }
+}
+</script>
+
+<style lang="scss">
+
+.auth-container {
+    padding: 0 0 0 20.625rem;
+    overflow: hidden;
+    display: flex;
+     .auth-logo {
+        margin-top : 1.5rem;
+    }
+}
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as FirstView } from './FirstView/index.vue';
 export { default as SignUp } from './SignUp/index.vue';
+export { default as Login } from './Login/index.vue'
