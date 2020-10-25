98190d82384c42120a56cc3b59f3cba82fae1753
Srim <deamoon0801@naver.com>
2020-10-25 17:03:07 +0900

    [CREATE] <components/PasswordReset/index.vue> component

---
+++ src/components/PasswordReset/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="auth-container">
+        <password-reset-auth @onPasswordAuth="onPassowrdReset" v-if="isPasswordAuth"></password-reset-auth>
+        <password-reset v-else></password-reset>
+        <side-view></side-view>
+    </div>
+</template>
+
+<script>
+import { PasswordResetAuth, PasswordReset, SideView } from './index';
+import '../../assets/style/authGlobal.scss';
+export default {
+    data () {
+        return {
+            isPasswordAuth : true
+        }
+    },
+    components : { 'password-reset-auth' : PasswordResetAuth, 'password-reset' : PasswordReset, 'side-view' : SideView },
+    methods : {
+        onPassowrdReset (data) {
+            this.isPasswordAuth = data;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
