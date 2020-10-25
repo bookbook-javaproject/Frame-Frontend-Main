ae24071e6503249ecc07ad4910ceed4d7cb0f7a5
Srim <deamoon0801@naver.com>
2020-10-25 17:00:00 +0900

    [CREATE] <components/PasswordReset/PasswordResetAuth.vue> add component

---
+++ src/components/PasswordReset/PasswordResetAuth.vue
@@ -0,0 +1,48 @@
+<template>
+    <main>
+        <header class="auth-header">
+            <img v-bind:src="frameLogo" alt="프레임-로고" class="auth-logo" />
+        </header>
+        <section class="auth-section">
+            <p>가입 시 등록한 이메일을 입력해주세요</p>
+            <h1>Password Reset</h1>
+            <form class="auth-form">
+                <div>
+                    <input type="text" placeholder="Email" v-model="email">
+                </div>
+                <span class="auth-error">{{ emailError }}</span>
+            </form>
+            <router-link to="/login" class="auth-link"><span class="point-link">로그인 페이지로 돌아가기</span></router-link>
+            <button @click="onPasswordAuth">비밀번호 재설정 코드 전송</button>
+        </section>
+    </main>
+</template>
+
+<script>
+import '../../assets/style/authGlobal.scss';
+export default {
+    data () {
+        return {
+            email : '',
+            emailError : '',
+            isPasswordAuth : true
+        }
+    },
+    methods : {
+        onPasswordAuth () {
+            if(this.email === '') {
+               this.emailError = '이메일을 입력해주세요.';
+            }
+            else {
+                this.isPasswordAuth = false;
+                this.$emit('onPasswordAuth', this.isPasswordAuth);
+                //this.$router.push('/passwordReset');
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
