4e966990fe305c56e79f3a1545c1d98d178ebc11
Srim <deamoon0801@naver.com>
2020-10-27 08:41:54 +0900

    [UPDATE] <router/index.js> router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { FirstView, SignUp, Login, MainPage, PasswordResetAuth, WriterApply } from '@/components';
+import { FirstView, SignUp, Login, MainPage, PasswordResetAuth, WriterApply, PoembookApply } from '@/components';
 import {TrendingPostList, RecentPostList, NoticePostList} from '@/components/Post/index.js';
 
 Vue.use(Router);
@@ -64,6 +64,11 @@ export default new Router({
       name : 'writerApply',
       path : '/writerApply',
       component : WriterApply
+    }, 
+    {
+      name : 'poembookApply',
+      path : '/poembookApply',
+      component : PoembookApply
     }
   ],
 });
