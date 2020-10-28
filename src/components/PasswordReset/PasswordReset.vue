a9137b3e949aee04f87f493a3a6595894b3dbb39
Srim <deamoon0801@naver.com>
2020-10-28 11:55:09 +0900

    [ADD] <components/PasswordReset/PasswordReset.vue> add complete UI

--- src/components/PasswordReset/PasswordReset.vue
+++ src/components/PasswordReset/PasswordReset.vue
@@ -1,54 +1,74 @@
 <template>
-    <main>
-        <header class="auth-header">
-            <img v-bind:src="frameLogo" alt="프레임-로고" class="auth-logo" />
-        </header>
-        <section class="auth-section">
-            <p>이메일로 발송된 비밀번호 재설정 코드를 입력해주세요.</p>
-            <h1>Password Reset</h1>
-            <form class="auth-form">
-                <div>
-                    <input type="text" placeholder="Password Reset Code" v-model="resetCode">
-                </div>
-                <span class="auth-error">{{ codeError }}</span>
-                <div>
-                    <input type="password" placeholder="New Password" v-model="password">
-                </div>
-                <span class="auth-error">{{ passwordError }}</span>
-            </form>
-            <router-link to="/login" class="auth-link"><span class="point-link">로그인 페이지로 돌아가기</span></router-link>
-            <button @click="onPasswordReset">비밀번호 재설정</button>
-        </section>
-    </main>
+  <main>
+    <header class="auth-header">
+      <img v-bind:src="frameLogo" alt="프레임-로고" class="auth-logo" />
+    </header>
+    <section class="auth-section" v-if="isPasswordReset">
+      <p>이메일로 발송된 인증 코드를 확인해주세요.</p>
+      <h1>Password Reset</h1>
+      <form class="auth-form">
+        <div>
+          <input
+            type="text"
+            placeholder="Password Reset Code"
+            v-model="resetCode"
+          />
+        </div>
+        <span class="auth-error">{{ codeError }}</span>
+        <div>
+          <input
+            type="password"
+            placeholder="New Password"
+            v-model="password"
+          />
+        </div>
+        <span class="auth-error">{{ passwordError }}</span>
+      </form>
+      <router-link to="/login" class="auth-link"><span class="point-link">로그인 페이지로 돌아가기</span></router-link>
+      <button @click="onPasswordReset">비밀번호 재설정</button>
+    </section>
+    <section class="auth-section" v-else>
+      <p>비밀번호 재설정이 완료되었습니다.</p>
+      <h1>Password Reset</h1>
+      <div class="complete-box">
+        <img :src="passwordReset" alt="비밀번호재설정-완료">
+      </div>
+      <router-link to="/" class="auth-link"><span class="point-link">메인 페이지로 돌아가기</span></router-link>
+    </section>
+  </main>
 </template>
 
 <script>
-import '../../assets/style/authGlobal.scss';
+import { frameLogo, passwordReset } from "@/assets/img";
+import "../../assets/style/authGlobal.scss";
 export default {
-    data () {
-        return {
-            resetCode : '',
-            password : '',
-            codeError : '',
-            passwordError : ''
-        }
+  data() {
+    return {
+      frameLogo,
+      passwordReset,
+      resetCode: "",
+      password: "",
+      codeError: "",
+      passwordError: "",
+      isPasswordReset: true,
+    };
+  },
+  methods: {
+    onPasswordReset() {
+      if (this.resetCode === "") {
+        this.codeError = "인증코드가 올바르지 않습니다.";
+      } else {
+        this.codeError = "";
+      }
+      if (this.password.length < 8) {
+        this.passwordError = "비밀번호는 8자 이상이어야합니다.";
+      } else {
+        this.passwordError = "";
+      }
+      if (this.codeError === "" && this.passwordError === "") {
+        this.isPasswordReset = false;
+      }
     },
-    methods : {
-        onPasswordReset () {
-            if(this.resetCode === '') {
-                this.codeError = '인증코드가 올바르지 않습니다.';
-            }
-            else {
-                this.codeError = '';
-            }
-
-            if(this.password.length < 8) {
-                this.passwordError = '비밀번호는 8자 이상이어야합니다.';
-            }
-            else {
-                this.passwordError = '';
-            }
-        }
-    }
-}
+  },
+};
 </script>
