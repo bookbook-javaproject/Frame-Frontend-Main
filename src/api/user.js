5fbfc1ac7095104fcd33b6a577ff1fe06f5da180
Srim <deamoon0801@naver.com>
2020-11-05 19:30:05 +0900

    [ADD] <api/user.js> signUp api

--- src/api/user.js
+++ src/api/user.js
@@ -11,3 +11,9 @@ export const setAuthInHeader = (accessToken, refreshToken) => {
   axios.defaults.headers.common["Authorization"] = accessToken ? `${accessToken}` : null;
   axios.defaults.headers.common["Authorization"] = refreshToken ? `${refreshToken}` : null;
 };
+
+export const signUp = (email, nickname, password) => {
+    return client.post("/user", { email, nickname, password })
+    .then((res)=> console.log(res))
+    .catch((err) => console.log(err));
+};
