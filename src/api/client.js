aed23e6000e0dbb0041f906bb5b770ce7576b0f1
Srim <deamoon0801@naver.com>
2020-11-16 22:35:51 +0900

    [ADD] <api/clinet.js> getClientAccessToken api

--- src/api/client.js
+++ src/api/client.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-const client = axios.create({
+export const client = axios.create({
   baseURL: "baseUrl",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export default client;
+export const getClientAccessToken = axios.create({
+    baseURL: "baseUrl",
+    headers: {
+        Authorization : localStorage.getItem('accessToken'),
+    },
+  });
