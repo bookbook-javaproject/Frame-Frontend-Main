94d150f2a0fca3856481bcc0bd7fe46eba64d819
Srim <deamoon0801@naver.com>
2020-11-19 23:14:17 +0900

    [UPDATE] <api/client.js> getClientAccessToken api

--- src/api/client.js
+++ src/api/client.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "baseUrl",
+  baseURL: process.env.VUE_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const getClientAccessToken = axios.create({
-    baseURL: "baseUrl",
-    headers: {
-        Authorization : localStorage.getItem('accessToken'),
-    },
-  });
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
+});
