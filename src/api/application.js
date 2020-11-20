4890c7500a775a20dcea7bf960585284904d98e1
Srim <deamoon0801@naver.com>
2020-11-20 10:22:51 +0900

    [ADD] <api/application.js> poembookApplycation api

--- src/api/application.js
+++ src/api/application.js
@@ -1,6 +1,12 @@
-import { uploadFile} from "./client";
+import { getClientAccessToken, uploadFile} from "./client";
 
 export const fileUpload = (file) => {
     return uploadFile.post("/file", {file})
     .then(({data}) => data)
+};
+
+export const poembookApplycation = (link, fileId) => {
+    return getClientAccessToken.post("/submission/anthology", { link, fileId })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
 };
