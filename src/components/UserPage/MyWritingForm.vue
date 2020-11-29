218064b497e2023d936a8149a09f50bafc2d7fa3
최종근 <zofqofhtltm2@gmail.com>
2020-11-30 07:25:43 +0900

    [CREATE] <src/components/UserPage/MyWritingForm.vue>

---
+++ src/components/UserPage/MyWritingForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="MyWritintForm-container">
+        <post-item v-for="(item,index) in items" v-bind:key="index" :post="item" />
+    </div>
+</template>
+
+<script>
+    import postItem from '../Post/PostItem';
+    import {mapActions,mapState} from 'vuex'
+export default {
+    components:{postItem},
+            data(){
+      return{
+        items: null
+      }
+    },
+    methods:{
+        ...mapActions([
+            'GET_MYPOST'
+        ])
+    },
+    computed: mapState([
+        'myPost'
+    ]),
+    async created(){
+        const item =  await this.GET_MYPOST({accessType:"public"});
+       this.items = item.data.userPostPreviews,
+       console.log(this.items);
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
