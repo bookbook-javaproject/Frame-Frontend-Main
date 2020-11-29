1144f9a5083458930638ce6fc01ba022991c4364
최종근 <zofqofhtltm2@gmail.com>
2020-11-30 07:23:41 +0900

    [error] not my file

--- src/components/SignUp/SignUpCodeCheck.vue
+++ src/components/SignUp/SignUpCodeCheck.vue
@@ -1,35 +1,62 @@
 <template>
-    <main>
-        <header class="auth-header">
-            <img v-bind:src="frameLogo" alt="프레임-로고" class="auth-logo" />
-        </header>
-        <section class="auth-section">
-            <p>등록된 이메일 주소로 전송된 인증 코드를 입력하세요.</p>
-            <h1>Sign Up</h1>
-            <form class="auth-form">
-                <div>
-                    <input type="text" placeholder="Code">
-                </div>
-            </form>
-            <router-link to="/signUp" class="auth-link"><span class="point-link">회원가입 페이지로 돌아가기</span></router-link>
-            <button>다음으로</button>
-        </section>
-        </main>
+  <main>
+    <header class="auth-header">
+      <img v-bind:src="frameLogo" alt="프레임-로고" class="auth-logo" />
+    </header>
+    <section class="auth-section">
+      <p>등록된 이메일 주소로 전송된 인증 코드를 입력하세요.</p>
+      <h1>Sign Up</h1>
+      <article class="auth-form">
+        <div>
+          <input type="text" placeholder="Code" v-model="code" @keyup.enter="onSignUpCheck"/>
+        </div>
+        <span class="auth-error">{{ codeError }}</span>
+      </article>
+      <router-link to="/sign-up" class="auth-link"
+        ><span class="point-link">회원가입 페이지로 돌아가기</span></router-link
+      >
+      <button @click="onSignUpCheck">다음으로</button>
+    </section>
+  </main>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 import { frameLogo } from "@/assets/img";
-import '../../assets/style/authGlobal.scss';
+import "../../assets/style/authGlobal.scss";
 export default {
-    name : "SignUpCheckPage",
-    data () {
-        return {
-            frameLogo
-        }
+  name: "SignUpCheckPage",
+  data() {
+    return {
+      frameLogo,
+      code: "",
+      codeError: "",
+    };
+  },
+  computed : {
+        ...mapGetters([
+            "isSignUp"
+        ])
     },
-}
+  methods: {
+    ...mapActions(["SIGN_UP_CHECK"]),
+    onSignUpCheck() {
+      if (this.code === "") {
+        this.codeError = "인증코드를 입력하세요.";
+      } 
+      else {
+        this.SIGN_UP_CHECK({ code: this.code }).then(() => {
+            console.log(this.isSignUp);
+            if(this.isSignUp === true) this.$router.push("/login");
+            else if(this.isSignUp == 403) this.codeError = "인증코드가 잘못되었습니다."
+            else if(this.isSignUp == 409) {
+                alert("이미 있는 계정입니다.")
+                this.$router.push("/login");
+            }
+            else alert("회원가입에 실패하였습니다. 다시 시도하세요.")
+        });
+      }
+    },
+  },
+};
 </script>
-
-<style lang="scss">
-
-</style>
--- src/components/SignUp/index.vue
+++ src/components/SignUp/index.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="auth-container">
-        <sign-up />
-        <side-view />
-    </div>
+  <div class="auth-container">
+    <sign-up @onSignUp="onSignUpAuth" v-if="isSignUp" />
+    <sign-up-check v-else />
+    <side-view />
+  </div>
 </template>
 
 <script>
-import { SideView, SignUp } from './';
-import '../../assets/style/authGlobal.scss';
+import { SideView, SignUp, SignUpCheck } from "./";
+import "../../assets/style/authGlobal.scss";
 export default {
-    name : "SignUp",
-    data () {
-        return {
-        }
+  name: "SignUp",
+  data() {
+    return {
+      isSignUp: true,
+    };
+  },
+  components: {
+    "side-view": SideView,
+    "sign-up": SignUp,
+    "sign-up-check": SignUpCheck,
+  },
+  methods: {
+    onSignUpAuth(data) {
+      this.isSignUp = data;
     },
-    components : { 'side-view' : SideView, 'sign-up' : SignUp }
-}
+  },
+};
 </script>
-
-<style lang="scss">
-
-</style>
