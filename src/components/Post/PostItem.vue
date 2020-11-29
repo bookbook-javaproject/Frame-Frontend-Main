82c7223f593f4e787586680933c3f63c67c6e792
최종근 <zofqofhtltm2@gmail.com>
2020-11-30 07:22:34 +0900

    [update] <src/components/Post/PostItem.vue>

--- src/components/Post/PostItem.vue
+++ src/components/Post/PostItem.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="postItem-container">
+  <div class="postItem-container" v-on:click="setLocalUser">
     <div class="postItem-card-title">
-      <img class="postItem-userImage" :src="user.userProfilePicture" >
+      <img class="postItem-userImage" v-on:click="userImageClicked" alt="아직 구현 안됨" >
       <div class="postItem-card-subtitle">
         <p>
-            {{user.nickname}}
+            {{post.writer}}
         </p>
         <p style="color: #555555">
-            {{post.dateTime}}
+            {{createdAtDate}}
         </p>
       </div>
     </div>
-    <div class="postItem-card-content">
+    <div class="postItem-card-content" v-on:click="postItemClicked">
             {{post.content}}
     </div>
     <div class="postItem-card-ev"> <!-- ev means Evaluation-->
         <div class="postItem-card-ev-items" v-on:click="empth">
             <img :src="isEmotioned ? emotionButtonClicked :emotionButton " />
             <p>
-                {{post.emotionSum}}
+                {{post.hearts}}
             </p>
         </div>
         <div class="postItem-card-ev-items" >
             <img :src="commentButton" />
             <p>
-                {{post.comment}}
+                {{post.comments}}
             </p>
         </div>
     </div>
@@ -34,8 +34,11 @@
 
 <script>
 import {commentButton,emotionButton,emotionButtonClicked} from '@/assets/img'
+import router from '@/router';
+import {mapActions} from 'vuex';
 export default {
-    props:['post','user'],
+    props:['post'],
+
 
     data:function() {
         return {
@@ -43,13 +46,33 @@ export default {
                 commentButton,
                 emotionButton,
                 emotionButtonClicked,
-                isEmotioned: false
+                isEmotioned: false,
             };
         },
         methods:{
+            ...mapActions([
+                'PATCH_HEART'
+            ]),
+            setLocalUser: function(){
+                localStorage.setItem('postId',this.post.postId);
+                console.log("로컬스토리지 셋 됨");
+            },
             empth :function(){ // empth means  공감
-                console.log("empth버튼 눌림");
-                this.isEmotioned = !this.isEmotioned
+                this.isEmotioned = !this.isEmotioned;
+                this.PATCH_HEART();
+                
+            },
+            postItemClicked: function(){
+                router.push(`/post/${localStorage.getItem('postId')}`);
+            },
+            userImageClicked: function(){
+                 router.push(`/userpage/${this.post.writerEmail}`)
+            }
+        },
+        computed:{
+            createdAtDate: function(){
+                const Date = this.post.createdAt.split('T')
+                return Date[0]
             }
         },
         created(){
@@ -120,3 +143,7 @@ export default {
 
     
 </style>    
+
+
+
+
