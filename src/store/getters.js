f2570a7e1aab85d352887ed5711f5260ed732455
최종근 <zofqofhtltm2@gmail.com>
2020-11-30 07:29:48 +0900

    [ERROR] 아무것도 안함

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,17 @@
-export default {};
+export default {
+  isAuth(state) {
+    return !!state.accessToken;
+  },
+  isFile(state) {
+    return state.isFile;
+  },
+  isPassword(state) {
+    return state.passwordError;
+  },
+  isSignUp(state) {
+    return state.userError;
+  },
+  isApplyError(state) {
+    return state.applyError;
+  }
+};
