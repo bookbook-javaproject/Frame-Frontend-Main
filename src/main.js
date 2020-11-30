b8cd264358f0e42a6dd4841a196bc3fe424a6550
_uchanlee <uchanlee.dev@gmail.com>
2020-11-30 22:34:19 +0900

    [ADD] Not Login handling

--- src/main.js
+++ src/main.js
@@ -11,6 +11,20 @@ if (localStorage.getItem('isFirstTime') !== 'false') {
   router.push('first');
 }
 
+const isAccessAble = ['/login', '/sign-up', '/first', '/password-reset'];
+
+router.beforeEach((to, _, next) => {
+  if (!localStorage.getItem('accessToken')) {
+    if (~isAccessAble.indexOf(to.fullPath)) {
+      next();
+    } else {
+      alert('로그인 후 이용해 주세요.')
+      router.push('/login');
+    }
+  } else {
+    next();
+  }
+});
 
 
 new Vue({
