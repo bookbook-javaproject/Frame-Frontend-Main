6ce32c0e19a2326a4555f2b42fc7dcf5e7891482
_uchanlee <uchanlee.dev@gmail.com>
2020-12-01 08:35:16 +0900

    [UPDATE] axios 인스턴스 방식 수정

--- src/api/follow.js
+++ src/api/follow.js
@@ -2,5 +2,5 @@ import { getClientAccessToken } from "./client";
 
 
 export const getFollowNumber = (email)=>{
-     return getClientAccessToken.get(`/user/relation/number?email=${email}`)
+     return getClientAccessToken().get(`/user/relation/number?email=${email}`);
 }
