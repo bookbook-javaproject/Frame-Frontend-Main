122472e28a416f52dac239f401c46451d7dc44c6
최종근 <zofqofhtltm2@gmail.com>
2020-11-30 09:29:04 +0900

    [create] <src/components/Post/PICForm.vue>

---
+++ src/components/Post/PICForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <PICommentForm v-bind:posts_detail="posts_detail" ></PICommentForm>
+</template>
+
+<script>
+import {mapState, mapActions} from 'vuex';
+import PICommentForm from './PICommentForm';
+
+export default {
+    components:{PICommentForm},
+    methods:{
+        ...mapActions([
+            'GET_POST_DETAIL'
+        ])
+    },
+    computed: mapState({
+          posts_detail: state=>state.posts_detail
+    }),
+      async created(){
+        localStorage.setItem('postId',this.$route.params.id)  
+        console.log("실행됨");
+        await this.GET_POST_DETAIL();
+  },
+
+}
+</script>
+
+<style>
+
+</style>
