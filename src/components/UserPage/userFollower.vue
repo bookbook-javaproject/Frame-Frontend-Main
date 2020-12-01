4fb0f771d8f1c7aa7ec3ce40903ed240569a6d27
_uchanlee <uchanlee.dev@gmail.com>
2020-12-01 21:34:34 +0900

    [ADD] 팔로우 수 연동

--- src/components/UserPage/userFollower.vue
+++ src/components/UserPage/userFollower.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="userFollowing-container-2">
-        <div v-for="(item,index) of items" v-bind:key="index">
+        <h3 v-if="follow && !follow.follower.length">팔로워가 0명입니다.</h3>
+        <div v-for="(item,index) of follow && follow.follower" v-bind:key="index">
             <FollowItem  v-bind:user="item" />
             <!-- <FollowItem  v-bind:user="items[index*2+1]" /> -->
         </div>
@@ -10,63 +11,17 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import FollowItem  from './FollowItem.vue'
 export default {
     components:{FollowItem},
-    data(){
-        return{
-            items:[
-                {
-                    following: 1,
-                    follower:4
-                },
-                {
-                    following: 2,
-                    follower:42
-                },
-                {
-                    following: 3,
-                    follower:434
-                },
-                {
-                    following: 4,
-                    follower:454
-                },
-                {
-                    following: 5,
-                    follower:74
-                },
-                {
-                    following: 6,
-                    follower:47
-                },
-                {
-                    following: 7,
-                    follower:41
-                },
-                {
-                    following: 8,
-                    follower:43
-                },
-                {
-                    following: 9,
-                    follower:49
-                },
-                                {
-                    following: 10   ,
-                    follower:49
-                }                                                                                          
-            ]
-        }
-    },
-    methods:{
-  
-    }
+    computed: mapState(['follow'])
 }
 </script>
 
 <style>
     .userFollowing-container-2{
+        margin-top: 1rem;
     }
     
 </style>
