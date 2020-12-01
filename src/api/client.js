b9ead7a9613b416873e57cd294e05c859a10a4a6
_uchanlee <uchanlee.dev@gmail.com>
2020-12-01 21:16:34 +0900

    [UPDATE] api file instance

--- src/api/client.js
+++ src/api/client.js
@@ -16,8 +16,6 @@ export const getClientAccessToken = () => axios.create({
 });
 
 export const uploadFile = axios.create({
-    baseURL : process.env.VUE_APP_FILE_API_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: process.env.VUE_APP_FILE_API_URL,
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
 });
